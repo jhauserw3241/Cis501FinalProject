4c8af464e386c5d788f2292a89f9b2f34fdb4f75
Joy Hauser <joy.hauser@hotmail.com>
2016-04-17 00:22:31 -0500

    Server Controller: Get the conversation part
    Get the participants from the conversation relationship
    with the given conversation name.

--- serverChat/serverChat/serverController.cs
+++ serverChat/serverChat/serverController.cs
@@ -25,5 +25,21 @@ namespace serverChat
             // Set the current user list to the model user list
             data.SetUserList(userList);
         }
+
+        // Get Conversation Participants
+        //
+        // Get the list of participants from the specified conversation
+        // @arg convName The name of the conversation
+        public List<string> GetConvParticipants(string convName)
+        {
+            // Get list of all conversation relationships
+            Dictionary<string, List<string>> convRelationships = data.GetContactRelationshipDict();
+
+            // Get the list of participants in the current conversation
+            List<string> participants = new List<string>();
+            convRelationships.TryGetValue(convName, out participants);
+
+            return participants;
+        }
     }
 }
