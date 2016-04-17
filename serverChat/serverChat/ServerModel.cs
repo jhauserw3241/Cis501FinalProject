86ce30bc1eafeb39a3fe889d92f5e895868b57a4
Joy Hauser <joy.hauser@hotmail.com>
2016-04-16 22:30:03 -0500

    Server Model: Add conversation list
    * Add the list of conversations
    * Add getConversationList() to get the list of all
    conversations
    * Add setConversationList() to set the list of all
    conversations

--- serverChat/serverChat/ServerModel.cs
+++ serverChat/serverChat/ServerModel.cs
@@ -8,5 +8,25 @@ namespace serverChat
 {
     class ServerModel
     {
+        // Declare the server lists
+        private List<serverConversation> conversationList = new List<serverConversation>();
+
+        // Get Conversation List
+        //
+        // Get the list of all conversations that have been created
+        // @return the list of server conversations
+        List<serverConversation> GetConversationList()
+        {
+            return conversationList;
+        }
+
+        // Set Conversation List
+        //
+        // Set the list of all conversations that have been created
+        // @arg list A list of conversations created since the server started
+        void SetConversationList(List<serverConversation> list)
+        {
+            conversationList = list;
+        }
     }
 }
