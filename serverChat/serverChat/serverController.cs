b4d55c4c7e375e25b51b0ebc22b328c8e75ec784
Joy Hauser <joy.hauser@hotmail.com>
2016-04-16 23:31:58 -0500

    Server Controller: Add contact to the list
    Add the specified contact to the list of all contacts
    in the server model class.

--- serverChat/serverChat/serverController.cs
+++ serverChat/serverChat/serverController.cs
@@ -8,5 +8,22 @@ namespace serverChat
 {
     public class ServerController
     {
+        private ServerModel data = new ServerModel();
+
+        // Add Contact To Model List
+        //
+        // Add the contact to the list of all of the users in the model
+        // @arg user The current user
+        public void AddContactToModelList(ServerUser user)
+        {
+            // Get the list of all contacts from the model
+            List<ServerUser> userList = data.GetUserList();
+
+            // Add the current user object
+            userList.Add(user);
+
+            // Set the current user list to the model user list
+            data.SetUserList(userList);
+        }
     }
 }
