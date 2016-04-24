84804b91edd70f4b383fce3bfb80ed59456062ef
Ryan Huber <rahuber@ksu.edu>
2016-04-24 14:14:30 -0500

    Client Model/ Conversation Class/ Contact Class

---
+++ Model/ClassLibrary1/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ClientConversation
+    {
+        private string convoName;
+        private List<string> participants = new List<string>();
+
+        public string ConvoName
+        {
+            get
+            {
+                return convoName;
+            }
+            set
+            {
+                convoName = value;
+            }
+        }
+
+        public List<string> Participants
+        {
+            get
+            {
+                return participants;
+            }
+            set
+            {
+                participants = value;
+            }
+        }
+
+    }
+}
---
+++ Model/Model/ModeCleint.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class ModelCleint
+    {
+        private int status;
+        private string ipAdd;
+        private string dispName;
+        private List<string> convs = new List<string>();
+        private Dictionary<string, bool> contList = new Dictionary<string, bool>();
+
+        public int Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+            }
+        }
+
+        public string IpAdd
+        {
+            get
+            {
+                return ipAdd;
+            }
+            set
+            {
+                ipAdd = value;
+            }
+        }
+
+        public string DispName
+        {
+            get
+            {
+                return dispName;
+            }
+            set
+            {
+                dispName = value;
+            }
+        }
+
+        public List<string> Convs
+        {
+            get
+            {
+                return convs;
+            }
+            set
+            {
+                convs = value;
+            }
+        }
+
+        public Dictionary<string, bool> ContList
+        {
+            get
+            {
+                return contList;
+            }
+            set
+            {
+                contList = value;
+            }
+        }
+    }
+}
