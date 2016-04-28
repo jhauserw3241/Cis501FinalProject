1f0f8d11b1c7551814d043f9bf9646f235d2b1de
amatkins <anth.mich.atki@gmail.com>
2016-04-27 20:18:15 -0500

    Client-side Integration
    Integrated separate parts of the client side.

--- FinalProjectChatClient/FinalProjectChatClient/ChatClientModel.cs
+++ FinalProjectChatClient/FinalProjectChatClient/ChatClientModel.cs
@@ -1,84 +1,38 @@
-﻿using System;
+﻿/* ChatClientModel.cs
+ * Author: Ryan Huber
+ * 
+ * A class representing the users information and the state of the program.
+ */
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FinalProjectChatClient
 {
     public class ChatClientModel
     {
-        #region Fields
-
-        private List<Contact> contactList;
-        private Dictionary<string, List<string>> conversationList;
-        private string displayName;
-        private bool errorFlag;
-        private string userName;
-        private FlowState state;
-        private string status;
-        private bool waitFlag;
-
-        #endregion
 
         #region Properties
 
-        public List<Contact> ContactList
-        {
-            get { return contactList; }
-            set { contactList = value; }
-        }
-        public Dictionary<string, List<string>> ConversationList
-        {
-            get { return conversationList; }
-            private set { conversationList = value; }
-        }
-        public string DisplayName
-        {
-            get { return displayName; }
-            set { displayName = value; }
-        }
-        public bool ErrorFlag
-        {
-            get { return errorFlag; }
-            set { errorFlag = value; }
-        }
-        public string Username
-        {
-            get { return userName; }
-            set { userName = value; }
-        }
-        public FlowState State
-        {
-            get { return state; }
-            set { state = value; }
-        }
-        public string Status
-        {
-            get { return status; }
-            set { status = value; }
-        }
-        public bool WaitFlag
-        {
-            get { return waitFlag; }
-            set { waitFlag = value; }
-        }
+        public List<Contact> ContactList { get; set; }
+        public Dictionary<string, List<string>> ConversationList { get; set; }
+        public string DisplayName { get; set; }
+        public bool ErrorFlag { get; set; }
+        public string Username { get; set; }
+        public FlowState State { get; set; }
+        public string Status { get; set; }
+        public bool WaitFlag { get; set; }
 
         #endregion
-
-        #region Public Methods
-
+        
         public ChatClientModel()
         {
-            contactList = new List<Contact>() { new Contact("admin", "Admin") };
-            conversationList = new Dictionary<string, List<string>>();
-            displayName = "";
-            userName = "";
-            state = FlowState.Entry;
-            status = "Offline";
-            waitFlag = false;
+            ContactList = new List<Contact>() { new Contact("admin", "Admin", "Online") };
+            ConversationList = new Dictionary<string, List<string>>();
+            DisplayName = "";
+            Username = "";
+            State = FlowState.Entry;
+            Status = "Offline";
+            WaitFlag = false;
+            ErrorFlag = false;
         }
-
-        #endregion
     }
 }
