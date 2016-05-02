5f0ae6664da9b2698c38bb06ee8190970b4a1195
amatkins <anth.mich.atki@gmail.com>
2016-05-02 18:37:58 -0500

    Client: Contact Test Update
    Fixed an instantiation issue with the Contact class tests.

--- FinalProjectChatClient/UnitTestProject1/ContactTest.cs
+++ FinalProjectChatClient/UnitTestProject1/ContactTest.cs
@@ -10,35 +10,35 @@ namespace ChatClientTest
         [TestMethod]
         public void ContactUsernameTest()
         {
-            Contact cnt = new Contact("Joy", "Joy_KSU");
+            Contact cnt = new Contact("Joy", "Joy_KSU", "Online");
             Assert.AreEqual("Joy", cnt.Username);
         }
 
         [TestMethod]
         public void ContactDisplayTest()
         {
-            Contact cnt = new Contact("Joy", "Joy_KSU");
+            Contact cnt = new Contact("Joy", "Joy_KSU", "Online");
             Assert.AreEqual("Joy_KSU", cnt.DisplayName);
         }
 
         [TestMethod]
         public void ContactStatusTest()
         {
-            Contact cnt = new Contact("Joy", "Joy_KSU");
+            Contact cnt = new Contact("Joy", "Joy_KSU", "Online");
             Assert.AreEqual("Online", cnt.Status);
         }
 
         [TestMethod]
         public void ContactToStringTest()
         {
-            Contact cnt = new Contact("Joy", "Joy_KSU");
+            Contact cnt = new Contact("Joy", "Joy_KSU", "Online");
             //Assert.AreEqual("",);
         }
 
         [TestMethod]
         public void ContactDisplaySetTest()
         {
-            Contact cnt = new Contact("Joy", "Joy_KSU");
+            Contact cnt = new Contact("Joy", "Joy_KSU", "Online");
             cnt.DisplayName = "Joy_CIS";
             Assert.AreEqual("Joy_CIS", cnt.DisplayName);
         }
@@ -46,7 +46,7 @@ namespace ChatClientTest
         [TestMethod]
         public void ContactStatusSetTest()
         {
-            Contact cnt = new Contact("Joy", "Joy_KSU");
+            Contact cnt = new Contact("Joy", "Joy_KSU", "Online");
             cnt.Status = "Offline";
             Assert.AreEqual("Offline", cnt.Status);
         }
